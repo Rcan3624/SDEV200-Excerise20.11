7771e06a8c5a3a320efba7bf9221a5280d9db5eb
Rcan3624 <55251953+Rcan3624@users.noreply.github.com>
2024-11-19 15:35:10 -0500

    Add files via upload

--- src/GroupSymbolCheck.java
+++ src/GroupSymbolCheck.java
@@ -1,18 +1,63 @@
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;
 import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.Stack;
 
 // TODO: Write a list or a regular expression or just something to check if the java source file has the proper grouping symbols such as () {} []
 
+
+
 public class GroupSymbolCheck {
+
+    public static boolean isBalanced(String expression) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : expression.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty()) {
+                    return false; // Unmatched closing bracket
+                }
+
+                char top = stack.pop();
+                if (!isMatchingPair(top, c)) {
+                    return false; // Mismatched brackets
+                }
+            }
+        }
+
+        return stack.isEmpty(); // If stack is empty, all brackets are matched
+    }
+
+    private static boolean isMatchingPair(char opening, char closing) {
+        return (opening == '(' && closing == ')') ||
+                (opening == '{' && closing == '}') ||
+                (opening == '[' && closing == ']');
+    }
+
     public static void main(String[] args) {
         try {
             File myObj = new File("TestCode.java");
             Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
+            String fileData = myReader.nextLine();
+            isBalanced(fileData);
+
+
+
+
+//            while (myReader.hasNextLine()) {
+//
+//                System.out.println(data);
+//
+//                if (data.equals("// Couldn't get the Java file writer to create a file inside the src folder. Just move this manually when you want to run the code")) {
+//                    System.out.println("String detection passed");
+//                } else if (data.equals("}")) {
+//                    System.out.println("String detection 2 passed");
+//                } else {
+//                    System.out.println("Nothing detected");
+//                }
+//            }
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
